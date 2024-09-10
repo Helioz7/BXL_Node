bb7cfc09e9daa3b64756419c56f5ce2ed3074f18
Helioz7 <baris.aktas@cf2000.onmicrosoft.com>
2024-09-10 16:17:32 +0200

    envois

--- index.js
+++ index.js
@@ -38,6 +38,18 @@ app.get('/link',function(requete,reponse){
     reponse.render('liens.ejs', {nomPage: 'Liens'});
 });
 
+/* définition du middelware qui va analyser la requête POST */
+let urlencodedParser = express.urlencoded({extended:false});
+
+/* Récupération des données et affichage de la page de traitement */
+app.post('/merci', urlencodedParser, function(req, res, next){
+  let lenom = req.body.nom;
+  let lemessage = req.body.msg;
+
+  res.render('merci.ejs',{titre:'formulaire reçu', nom:lenom, msg:lemessage});
+});
+
+
 // définir une "page" gérant l'erreur 404
 app.use(function(requete,reponse,next){
     reponse.status(404).render('page-404.ejs', {nomPage: 'Erreur 404'});
@@ -45,3 +57,4 @@ app.use(function(requete,reponse,next){
 
 app.listen(8080);       // le serveur web écoute sur le port 8080
 console.log("Express est démarré et attend vos requêtes...");
+
